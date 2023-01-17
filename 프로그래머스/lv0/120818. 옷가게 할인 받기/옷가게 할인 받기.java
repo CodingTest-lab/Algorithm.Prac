896639a1ebaa4ca790c2384b5b5323bebe5b69a1
SungWonSong <117096035+SungWonSong@users.noreply.github.com>
2023-01-17 11:16:13 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int price) {
+        if(price>=100000&&price<300000){
+            double answer =price * 0.95;
+            return (int)answer; 
+        }
+        else if(price>=300000&&price<500000){
+            double answer= price * 0.9;
+            return (int)answer;
+        }
+        else if(price>=500000&&price<=1000000){
+            double answer= price * 0.8;
+            return (int)answer;
+        }
+        else{ 
+            return price;
+        } 
+    } 
+}
