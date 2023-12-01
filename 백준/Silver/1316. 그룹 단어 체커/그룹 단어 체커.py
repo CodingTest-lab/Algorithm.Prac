254543391eabc7f85480037085b0ebedd451e126
SungWonSong <117096035+SungWonSong@users.noreply.github.com>
2023-12-01 12:41:43 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,28 +1,11 @@
-def is_group_word(word):
-    visited = set()  # 이미 방문한 문자를 기록하기 위한 집합
+num = int(input())
+count = 0
 
-    for i in range(len(word)):
-        if word[i] not in visited:  # 처음 나타난 문자인 경우
-            visited.add(word[i])
-        else:
-            if word[i] != word[i - 1]:  # 바로 앞 문자와 다르면 그룹 단어가 아님
-                return False
-
-    return True
-
-def count_group_words(words):
-    count = 0
-
-    for word in words:
-        if is_group_word(word):
+for _ in range (num):
+    word = input()
+    for i in range (len(word)-2): 
+        if word[i] != word[i+1] and word[i] in word[i+2:]:
             count += 1
-
-    return count
-
-# 입력 처리
-N = int(input())
-word_list = [input() for _ in range(N)]
-
-# 결과 출력
-result = count_group_words(word_list)
-print(result)
+            break
+            
+print(num - count)
