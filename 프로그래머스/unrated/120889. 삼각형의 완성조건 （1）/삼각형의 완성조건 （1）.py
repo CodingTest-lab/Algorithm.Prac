e5edb077f05046665dfe2b1e01f130a0425c253a
SungWonSong <117096035+SungWonSong@users.noreply.github.com>
2023-12-15 03:40:32 +0900

    [level unrated] Title: 삼각형의 완성조건 (1), Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.py
+++ 프로그래머스/unrated/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.py
@@ -1,9 +1,9 @@
 def solution(sides):
     sides.sort()
-    if sides[0] + sides[1] > sides[2]:
+    if sides[0]+sides[1] > sides[2]:
         return 1
     else:
         return 2
     
-
+    
     
